eeb69185c06950d31aae6f7977b59f745b844b2d
Minh Le <mqle@knox.edu>
2019-03-28 16:35:33 -0500

    1st commit
    Added and Populated User database

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'user' => 'users#user'
+  get 'user/new' => 'users#new'
+  get 'listing' => 'listing#listing'
+  resource :users
 end
